41e225b05c5ec5240b08e61c2d4074302f64a088
Leigh McLean <lemlea@au1.ibm.com>
2019-04-02 19:38:15 +1000

    Initial Commit

---
+++ pcars_190402/pcars_190402.h
@@ -0,0 +1,19 @@
+//
+//  pcars_190402.h
+//  pcars_190402
+//
+//  Created by Leigh McLean on 2/4/19.
+//  Copyright © 2019 Leigh McLean. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for pcars_190402.
+FOUNDATION_EXPORT double pcars_190402VersionNumber;
+
+//! Project version string for pcars_190402.
+FOUNDATION_EXPORT const unsigned char pcars_190402VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pcars_190402/PublicHeader.h>
+
+
