e080dbd27f52f0e7b7541d976f1f0ec8b00d6326
Leigh McLean <lemlea@au1.ibm.com>
2019-04-02 19:55:49 +1000

    add reader protocol

---
+++ pcars_190402/Reader.swift
@@ -0,0 +1,14 @@
+//
+//  Reader.swift
+//  pcars_190402
+//
+//  Created by Leigh McLean on 2/4/19.
+//  Copyright © 2019 Leigh McLean. All rights reserved.
+//
+
+import Foundation
+
+protocol Reader
+{
+    func read() -> Array<UInt8>
+}
